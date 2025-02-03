1a5fd09799f040c8c9fc93e0f503492611ff57e8
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-03 17:01:02 +0530

    add DoctorSlice.ts  to Store.ts

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import CustomerSlice from "../reducers/CustomerSlice.ts";
+import DoctorSlice from "../reducers/DoctorSlice.ts";
 
 
 
 const store = configureStore({
     reducer: {
         customers: CustomerSlice,
+        doctors: DoctorSlice,
     },
 });
 
