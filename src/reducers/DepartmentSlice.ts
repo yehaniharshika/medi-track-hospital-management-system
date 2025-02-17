23b91c5f3314fd96c23fda6e076c92e4f71dafd4
yehaniharshika <pamunuwayehaniharshika@gmail.com>
2025-02-18 01:07:47 +0530

    successfully created DepartmentSlice.ts

--- src/reducers/DepartmentSlice.ts
+++ src/reducers/DepartmentSlice.ts
@@ -1,9 +1,60 @@
 import {Department} from "../models/Department.ts";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import axios from "axios";
 
 export const initialState: Department[] = [];
 
+const api = axios.create({
+    baseURL : "http://localhost:3003/department",
+})
+
+export const saveDepartment = createAsyncThunk(
+    "department/saveDepartment",
+    async (department : Department) => {
+        try {
+            const response = await api.post('/add',department);
+            return response.data;
+        }catch (error) {
+            console.error("Error saving Department:", error);
+        }
+    }
+);
+
+export const deleteDepartment = createAsyncThunk(
+    "department/deleteDepartment",
+    async (departmentId : string) => {
+        try {
+            const response = await api.delete(`/delete/${departmentId}`);
+            return response.data;
+        }catch (error) {
+            console.error("Error deleting Department:", error);
+        }
+    }
+);
+
+export const updateDepartment = createAsyncThunk(
+    "department/updateDepartment",
+    async (department : Department) =>{
+        try {
+            const response = await api.put(`/update/${department.departmentId}`,department);
+            return response.data;
+        }catch (error){
+            console.error("Error updating Department:", error);
+        }
+    }
+);
+
+export const getDepartments = createAsyncThunk(
+    "department/getDepartments",
+    async () =>{
+        try {
+            const response = await api.get('/view');
+            return response.data;
+        }catch (error){
+            console.error("Error getting Departments: ", error);
+        }
+    }
+);
 
 const departmentSlice = createSlice({
     name: 'department',
@@ -13,7 +64,7 @@ const departmentSlice = createSlice({
             state.push(action.payload);
         },
 
-        updateDepartment: (state, action: PayloadAction<Department>) => {
+        updatedDepartment: (state, action: PayloadAction<Department>) => {
             const index = state.findIndex((department) => department.departmentId === action.payload.departmentId
             );
 
@@ -22,12 +73,69 @@ const departmentSlice = createSlice({
             }
         },
 
-        deleteDepartment: (state, action: PayloadAction<string>) => {
+        deletedDepartment: (state, action: PayloadAction<string>) => {
             return state.filter((department) => department.departmentId !== action.payload);
         },
     },
+    extraReducers: (builder) => {
+        builder
+            .addCase(saveDepartment.fulfilled, (state, action) => {
+                state.push(action.payload);
+            })
+
+            .addCase(saveDepartment.pending, (_, action) => {
+                console.error("Pending save Department: ",action.payload);
+            })
+
+            .addCase(saveDepartment.rejected, (_, action) => {
+                console.error("Failed to save Department: ", action.payload);
+            });
+
+        builder
+            .addCase(deleteDepartment.fulfilled, (state, action) => {
+                return state = state.filter((department : Department)=> department.departmentId !== action.payload.departmentId);
+            })
+
+            .addCase(deleteDepartment.rejected, (_, action) => {
+                console.error("Failed to delete departmentId: ", action.payload);
+            })
+
+            .addCase(deleteDepartment.pending, (_, action) => {
+                console.log("Pending delete department: ",action.payload);
+            });
+
+        builder
+            .addCase(updateDepartment.rejected, (_, action) => {
+                console.error("Failed to update department: ", action.payload);
+            })
+
+            .addCase(updateDepartment.fulfilled, (state, action) => {
+                const department = state.find((department:Department) => department.departmentId === action.payload.departmentId);
+                if (department) {
+                    department.departmentName = action.payload.departmentName;
+                    department.departmentEmail = action.payload.departmentEmail;
+                    department.location = action.payload.location;
+                    department.headOfDepartment = action.payload.headOfDepartment;
+                    department.phoneNumber = action.payload.phoneNumber;
+                }
+            })
+            .addCase(updateDepartment.pending, (_, action) => {
+                console.log("Pending update department:", action.payload);
+            });
+
+        builder
+            .addCase(getDepartments.fulfilled, (_, action) => {
+                return action.payload;
+            })
+            .addCase(getDepartments.pending, (_, action) => {
+                console.log("Pending get Department:", action.payload);
+            })
+            .addCase(getDepartments.rejected, (_, action) => {
+                console.error("Failed to get Department:", action.payload);
+            });
+    }
 
 });
 
-export const {addDepartment, updateDepartment, deleteDepartment} = departmentSlice.actions;
+export const {addDepartment, updatedDepartment, deletedDepartment} = departmentSlice.actions;
 export default departmentSlice.reducer;
